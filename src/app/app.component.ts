9031cf28622f876507b52318348704879858296b
Xianan Fan <xianan.fan@teradata.com>
2017-10-09 14:24:58 -0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
+  constructor(private auth: AuthService) {
   }
 
 }
